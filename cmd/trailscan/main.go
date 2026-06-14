de7934bd3e7138b00d47fa5b17b4c1160a335c2b
David Kröll <david.kroell@outlook.com>
2026-06-14 11:43:14 +0200

    implement all options which are offered

--- cmd/trailscan/main.go
+++ cmd/trailscan/main.go
@@ -2,10 +2,12 @@ package main
 
 import (
 	"context"
+	"encoding/json"
 	"errors"
 	"fmt"
 	"os"
 	"slices"
+	"text/tabwriter"
 
 	"github.com/davidkroell/trailscan"
 	"github.com/urfave/cli/v3"
@@ -16,8 +18,8 @@ func main() {
 		UsageText: "trailscan <track.gpx> [OPTIONS]",
 		Name:      "trailscan",
 		ArgsUsage: "<track.gpx>",
-		Usage:     "a tool for analyzing GPX tracks and enriching them with data from OpenStreetMap.",
-		Description: `trailscan is a tool for analyzing GPX tracks and enriching them with data from OpenStreetMap.
+		Usage:     "a tool for analyzing GPX tracks using data from OpenStreetMap.",
+		Description: `trailscan is a tool for analyzing GPX tracks using data from OpenStreetMap.
 It matches recorded GPS positions against geographic features to determine where a track passes and what locations were visited.
 The resulting track can be annotated with information such as mountain summits, landmarks, points of interest,
 and other geographic features encountered along the route.`,
@@ -35,6 +37,19 @@ and other geographic features encountered along the route.`,
 				Usage:   "endpoint to use to send the overpass query",
 			},
 			&cli.StringFlag{
+				Name:  "overpass-query-type",
+				Value: "peaks",
+				Usage: "uses a predefined query, supported queries are [peaks, villages, hiking, cycling]",
+				Validator: func(s string) error {
+					supported := []string{"peaks", "villages", "hiking", "cycling"}
+					if slices.Contains(supported, s) {
+						return nil
+					}
+
+					return errors.New("supported queries are [peaks, villages, hiking, cycling]")
+				},
+			},
+			&cli.StringFlag{
 				Name:    "output",
 				Aliases: []string{"o"},
 				Value:   "text",
@@ -48,41 +63,88 @@ and other geographic features encountered along the route.`,
 					return errors.New("supported output formats are [text, json]")
 				},
 			},
+			&cli.Float64Flag{
+				Name:    "max-distance",
+				Aliases: []string{"md"},
+				Value:   50,
+				Usage:   "configure the maximum distance between tracked and actual",
+				Validator: func(v float64) error {
+					if v < 1 || v > 1000 {
+						return errors.New("maximum distance must be between 1 and 1000")
+					}
+					return nil
+				},
+			},
+			&cli.Float64Flag{
+				Name:    "max-elevation-diff",
+				Aliases: []string{"me"},
+				Value:   30,
+				Usage:   "configure the maximum elevation difference between tracked and actual",
+				Validator: func(v float64) error {
+					if v < 1 || v > 1000 {
+						return errors.New("maximum elevation difference must be between 1 and 1000")
+					}
+					return nil
+				},
+			},
 		},
 		Action: func(ctx context.Context, cmd *cli.Command) error {
 			file, err := os.Open(cmd.StringArg("gpxfile"))
 			if err != nil {
-				panic(err)
+				return fmt.Errorf("cannot open gpx file: %w", err)
 			}
 			defer file.Close()
 
 			points, bbox, err := trailscan.LoadGPX(file)
 			if err != nil {
-				panic(err)
+				return fmt.Errorf("cannot load gpx file: %w", err)
 			}
 
-			fmt.Printf("Loaded %d track points\n", len(points))
+			fetchOptions := trailscan.DefaultFetchOptions()
+			fetchOptions.Endpoint = cmd.String("overpass-endpoint")
+			switch cmd.String("overpass-query-type") {
+			case "peaks":
+				fetchOptions.QueryTemplate = trailscan.PeaksQueryTemplate
+			case "villages":
+				fetchOptions.QueryTemplate = trailscan.VillagesQueryTemplate
+			case "hiking":
+				fetchOptions.QueryTemplate = trailscan.HikingQueryTemplate
+			case "cycling":
+				fetchOptions.QueryTemplate = trailscan.CyclingQueryTemplate
+			}
 
-			peaks, err := trailscan.FetchPeaks(bbox)
+			amenities, err := trailscan.FetchAmenities(ctx, bbox, fetchOptions)
 			if err != nil {
-				panic(err)
+				return fmt.Errorf("cannot fetch amenities: %w", err)
 			}
 
-			fmt.Printf("Found %d peaks in bounding box\n", len(peaks))
+			findOpts := trailscan.DefaultFindOptions()
+			findOpts.MaxDistanceMeters = cmd.Float64("max-distance")
+			findOpts.MaxElevationDifference = cmd.Float64("max-elevation-diff")
 
-			visited := trailscan.FindVisitedPeaks(points, peaks)
+			visited := trailscan.FindVisitedAmenities(points, amenities, findOpts)
 
-			fmt.Println("\nVisited Peaks")
-			fmt.Println("=============")
+			switch cmd.String("output") {
+			case "text":
+				w := tabwriter.NewWriter(cmd.Writer, 0, 0, 2, ' ', 0)
+				_, _ = fmt.Fprintf(w, "NAME\tTYPE\tLAT\tLON\tREAL ELEVATION\tTRACKED ELEVATION\tDISTANCE\n")
 
-			for _, v := range visited {
-				fmt.Printf(
-					"%s | peak=%.0fm | track=%.0fm | distance=%.1fm\n",
-					v.Peak.Name,
-					v.Peak.Ele,
-					v.TrackElevation,
-					v.Distance,
-				)
+				for _, v := range visited {
+					_, _ = fmt.Fprintf(w,
+						"%s\t%s\t%0.5f\t%0.5f\t%.0fm\t%.0fm\t%.1fm\n",
+						v.Amenity.Name,
+						v.Amenity.Type,
+						v.Amenity.Lat,
+						v.Amenity.Lon,
+						v.Amenity.Ele,
+						v.TrackElevation,
+						v.Distance,
+					)
+				}
+
+				_ = w.Flush()
+			case "json":
+				_ = json.NewEncoder(cmd.Writer).Encode(visited)
 			}
 			return nil
 		},
